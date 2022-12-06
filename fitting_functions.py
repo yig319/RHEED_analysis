c65e112ae5b2ad252bb7d3ae5f2edce775857d58
Yichen Guo <yig319@lehigh.edu>
2022-12-05 21:20:54 -0500

    update the analysis notebook

--- fitting_functions.py
+++ fitting_functions.py
@@ -4,18 +4,20 @@ from scipy import signal
 from scipy.optimize import curve_fit
 from scipy.stats import zscore
 
+import sys
+sys.path.append('./')
+from visualize_functions import plot_curve, show_grid_plots
 
-def load_curve(h5_para_file, growth, spot, metric, camera_freq, x_start, figsize):
+def load_curve(h5_para_file, growth, spot, metric, camera_freq, x_start):
     h5_para = h5py.File(h5_para_file, mode='r')
-    curve_y = h5_para[growth][spot][metric][200:-200]
+    curve_y = np.array(h5_para[growth][spot][metric])
     curve_x = np.linspace(x_start, x_start+len(curve_y)-1, len(curve_y))/camera_freq
     return curve_x, curve_y
 
-def load_multiple_curves(h5_para_file, growth_dict, spot, metric, camera_freq=500, 
-                         x_start=0, interval=200, title=None, figsize=(12,2.5), save_path=None):
+def load_multiple_curves(h5_para_file, growth_dict, spot, metric, camera_freq=500, x_start=0, interval=200):
     x_all, y_all = [], []
     for growth_name in list(growth_dict.keys()):
-        curve_x, curve_y = load_curve(h5_para_file, growth_name, spot, metric, camera_freq, x_start, figsize=figsize)
+        curve_x, curve_y = load_curve(h5_para_file, growth_name, spot, metric, camera_freq, x_start)
         x_start = x_start+len(curve_y)+interval
         x_all.append(curve_x)
         y_all.append(curve_y)
@@ -23,7 +25,7 @@ def load_multiple_curves(h5_para_file, growth_dict, spot, metric, camera_freq=50
     x_all = np.concatenate(x_all)
     y_all = np.concatenate(y_all)
     return x_all, y_all
-    
+
 
 def color_curve(data, indicator, peaks=None):
     
@@ -75,15 +77,27 @@ def detect_peaks(curve_x, curve_y, camera_freq, laser_freq, step_size, prominenc
         ys.append(list(curve_y[5+x_peaks[i-1]:x_peaks[i]]))
     return x_peaks/500, xs, ys
 
-def remove_outlier(array, boundary, n=3):
-    for n_ in range(n):
-        z = zscore(array, axis=0, ddof=0)
-        for i in range(1, len(array)-1):
-            if z[i] < -np.abs(boundary) or z[i] > np.abs(boundary):
-                array[i] = (array[i-1]+array[i+1])/2
-    return array
+# def remove_outlier(array, boundary, n=3):
+#     for n_ in range(n):
+#         z = zscore(array, axis=0, ddof=0)
+#         for i in range(1, len(array)-1):
+#             if z[i] < -np.abs(boundary) or z[i] > np.abs(boundary):
+#                 array[i] = (array[i-1]+array[i+1])/2
+#     return array
 
-def fit_exp(xs, ys, tau_, x_peaks=[], ylim=None, camera_freq=500):
+
+def remove_outlier(x, y, boundary):
+    z = zscore(y, axis=0, ddof=0)
+    x = np.delete(x, np.where(z>boundary))
+    y = np.delete(y, np.where(z>boundary))
+    return x, y
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+      
+def fit_exp(xs, ys, p_init, x_peaks=[], ylim=None, camera_freq=500, add_previous=False):
     def exp_func(x, tau):
         return (1 - np.exp(-x/tau))
     
@@ -96,19 +110,25 @@ def fit_exp(xs, ys, tau_, x_peaks=[], ylim=None, camera_freq=500):
         
         # use I/I0, I0 is saturation intensity (last value) and scale to 0-1 based 
         n_avg = len(ys[i])//100+3
+        
+        # add previous data point
+        if i != 0 and add_previous:
+            ys[i][0] = I_end
+            
         I_start = np.mean(ys[i][:n_avg])
         I_end= np.mean(ys[i][-n_avg:])
-        
+            
         if I_end-I_start == 0: 
 #             print('I_end = I_start, intensity:', ys[i])
             ys[i] = (ys[i]-I_start) # I-Imin/Imax-Imin (I/I0)
         else:
             ys[i] = (ys[i]-I_start)/(I_end-I_start) # I-Imin/Imax-Imin (I/I0)
-        params, params_covariance = curve_fit(exp_func, x, ys[i], p0=tau_, bounds=[0.01, 1]) # curve_fit
+            
+        params, params_covariance = curve_fit(exp_func, x, ys[i], p0=p_init, bounds=[0.01, 1]) # curve_fit
         tau = params[0]
         y_fit = exp_func(x, tau)
         sign_list.append('1-exp(-t/tau)')
-        labels.append(f'index: {i+1};\nfitted: 1-exp(-t/{np.round(tau, 2)});\nstart at: {camera_freq*x_peaks[i]}')
+        labels.append(f'index: {i+1};\nfitted: 1-exp(-t/{np.round(tau, 2)})')
         
         ys[i] = list(ys[i])
         tau_list.append(np.round(tau, 4))
@@ -119,23 +139,162 @@ def fit_exp(xs, ys, tau_, x_peaks=[], ylim=None, camera_freq=500):
     return tau_list, labels, ys_fit, [sign_list, I_start_list, I_end_list]
 
 
-def analyze_curves(h5_para_file, growth_dict, spot, metric, camera_freq=500, title=None):
+
+def fit_exp_function(xs, ys, camera_freq=500, 
+                     fit_settings = {'fit_func': 'exp', 'I_diff': 5000, 'bounds':[0.01, 1], 'p_init':(1, 0.1),
+                                     'from_previous': False, 'weight_previous': False, 'relative_intensity': False}):
     
-    tau_list_all, Imin_list_all, Imax_list_all = [], [], []
+    '''
+    I_diff: Intensity difference used to normalize the curve to 0-1;
+    from_previous: int or False, the number of points used to add the I_end value in front of recovery curve;
+    weight_previous: int or False, weight for previous data points: n times of average weight;
+    
+    '''
+    def exp_func(x, a, b):
+        return a*(1 - np.exp(-x/b))
+    def linear_func(x, a, b):
+        return a*x+b    
+    
+    fit_func = fit_settings['fit_func']
+    I_diff = fit_settings['I_diff']
+    bounds = fit_settings['bounds']
+    p_init = fit_settings['p_init']
+    from_previous = fit_settings['from_previous']
+    weight_previous = fit_settings['weight_previous']
+    relative_intensity = fit_settings['relative_intensity']
+    
+    if not from_previous and weight_previous:
+        print("Please assign an integer to from_previous, otherwise, can't assign weight for previous data point.")
+        return 
+    
+    if fit_func == 'linear' and weight_previous:
+        print("Please note that the linear function will only be fitted with last 90% of data points of every curve, \
+so the first several data points will not contribute to the fitting process.")
+        
+    a_list, b_list = [], []
+    labels, ys_fit, sign_list, I_start_list, I_end_list = [], [], [], [], []
+    
+    for i in range(len(xs)):
+        x = np.linspace(1e-5, 1, len(ys[i])) # use second as x axis unit
+        n_avg = len(ys[i])//100+3
+        
+        
+        # section: normalize the curve
+        
+        # add previous data point
+        if i != 0 and from_previous:
+            ys[i][:from_previous] = [I_end]*from_previous
+            
+        I_start = np.mean(ys[i][:n_avg])
+        I_end = np.mean(ys[i][-n_avg:])
+
+        if relative_intensity:
+            # change the points from previous to be with relative intensity
+            if from_previous and ((I_start < I_end and ys[i][0] > I_start) or (I_start > I_end and ys[i][0] < I_start)):
+                if I_diff:
+                    I_delta = np.abs((ys[i][0]-I_start)) * I_diff/np.abs(I_end-I_start)
+                else:
+                    I_delta = np.abs((ys[i][0]-I_start))
+                ys[i][:from_previous] = [I_start - I_delta]*from_previous
+            I_start = np.mean(ys[i][:n_avg])
+        
+        
+        # use I/I0, I0 is saturation intensity (last value) and scale to 0-1 based 
+        if I_end - I_start == 0: # avoid devide by 0
+            ys[i] = (ys[i]-I_start)
+        elif I_diff: # use fixed I_diff
+            if I_end < I_start:
+                ys[i] = (ys[i]-I_start)/(-I_diff)
+            else:
+                ys[i] = (ys[i]-I_start)/I_diff # I-Imin/Imax-Imin (I/I0)
+
+                
+        # section: fit curve with function and collect data
+        if fit_func == 'linear':
+            params, params_covariance = curve_fit(linear_func, x[-int(len(x)*0.9):], ys[i][-int(len(x)*0.9):], p0=p_init) 
+            a, b = params
+            y_fit = linear_func(x, a, b)
+        
+        elif fit_func == 'exp':
+            # change weights for previous point
+            if from_previous and weight_previous:
+                yerr = 1/ys[i]
+                yerr[:from_previous]*=weight_previous
+                params, params_covariance = curve_fit(exp_func, x, ys[i], p0=p_init, sigma=yerr, bounds=[0.01, 1], absolute_sigma=True) 
+            else:
+                params, params_covariance = curve_fit(exp_func, x, ys[i], p0=p_init, bounds=[0.01, 1], absolute_sigma=True) 
+            a, b = params
+            y_fit = exp_func(x, a, b)
+        
+        ys[i] = list(ys[i])
+        a_list.append(np.round(a, 4))
+        b_list.append(np.round(b, 4))
+        
+        labels.append(f'index: {i+1};\nfitted: {np.round(a, 2)}*(1-exp(-t/{np.round(b, 2)});\nI_diff={I_diff})')
+        ys_fit.append(y_fit)
+        I_start_list.append(np.round(I_start, 4))
+        I_end_list.append(np.round(I_end, 4))
+
+    return a_list, b_list, [labels, ys_fit, sign_list, I_start_list, I_end_list]
+
+
+
+def analyze_curves(h5_para_file, growth_dict, spot, metric, camera_freq=500, interval=1000, visualize=False, 
+                   fit_settings={'fit_func': 'exp', 'I_diff': 8000, 'bounds':[0.01, 1], 'p_init':(1, 0.1),
+                                 'from_previous': 2, 'weight_previous': 1, 'relative_intensity': False}):
+    '''
+    h5_para_file: String, path of the h5 file name, containing information about the RHEED spots;
+    growth_dict: names of the growth index and corresponding frequency;
+    spot: String, name of the RHEED spot to collect, choice of "spot_1", "spot_2" or "spot_3"; 
+    metric: String, name of the metric to analyze the RHEED spot, choice of ...;
+    camera_freq: int, camera frequency of data acquisition. Default camera_freq is 500 fps;
+    interval=1000;
+    title=None;
+    upper_limit: int, the number of levels to smooth the curve by remove the outliers. Default is 0;
+    visualize=False;
+    fit_settings: setting parameters for fitting function
+    '''
+    
+    a_list_all, b_list_all, Imin_list_all, Imax_list_all, x_list_all = [], [], [], [], []
+    x_end = 0
     for growth_name in list(growth_dict.keys()):
+
         # load data
-        sample_x, sample_y = load_curve(h5_para_file, growth_name, 'spot_2', 'img_intensity', camera_freq=500, x_start=0, figsize=(12,4))
+        sample_x, sample_y = load_curve(h5_para_file, growth_name, 'spot_2', 'img_intensity', camera_freq=500, x_start=0)
 
         # detect peaks
         x_peaks, xs, ys = detect_peaks(sample_x, sample_y, camera_freq=camera_freq, laser_freq=growth_dict[growth_name],
                                        step_size=5, prominence=0.1)
-#         print(np.sum(x_peaks))
-
         # fit exponential function
-        tau_list, labels, ys_fit, info = fit_exp(xs, ys, tau_=0.1, x_peaks=x_peaks, ylim=None, camera_freq=camera_freq)
-        sign_list, I_start_list, I_end_list = info
-        tau_list_all+=tau_list
+        a_list, b_list, info = fit_exp_function(xs, ys, camera_freq=500, fit_settings=fit_settings)        
 
-    tau_list_all = remove_outlier(np.array(tau_list_all[2:]), 0.8, n=5)
+        labels, ys_fit, sign_list, I_start_list, I_end_list = info
+        b_list_all+=b_list
+        a_list_all+=a_list
+        
+        if x_list_all != []:
+            x_list = x_peaks[:-1] + x_end
+        else:
+            x_list = x_peaks[:-1]
+                    
+        if visualize:
+            if x_list_all != []:
+                sample_x += x_end
+                x_peaks += x_end
 
-    return tau_list_all
+            plot_curve(sample_x, sample_y, x_peaks=x_peaks, plot_type='scatter', xlabel='Time (s)', ylabel='Intensity (a.u.)',
+                       figsize=(12, 4))            
+            plot_curve(x_list, b_list, plot_type='lineplot', xlabel='Laser ablation (count)', ylabel='Characteristic Time (s)', 
+                       yaxis_style='linear', figsize=(12, 4))
+            plot_curve(x_list, a_list, plot_type='lineplot', xlabel='Laser ablation (count)', ylabel='Intensity Magnitude (a.u.)', 
+                       yaxis_style='linear', figsize=(12, 4))
+            show_grid_plots(xs, ys, labels, ys_fit, ylim=None)
+        
+        
+        x_end = round(x_end + (len(sample_x)+interval)/camera_freq, 2)
+        x_list_all.append(x_list)
+        
+    x_list_all = np.concatenate(x_list_all)[:len(b_list_all)]
+    b_list_all = np.array(b_list_all)
+    
+    return a_list_all, b_list_all, x_list_all
